b4a7d89b9c46886752efe0aec97ffb728e8ebd55
parth21999 <parth21999@gmail.com>
2020-06-11 11:29:41 -0400

    adding reals to pal

---
+++ linux/src/reals_linux/real_constbuffer_array_linux.c
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define GBALLOC_H
+
+#include "real_constbuffer_array_renames.h"
+#include "real_constbuffer_renames.h"
+
+#include "../src/constbuffer_array.c"
