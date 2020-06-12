031003e6c9a580e26961fadde23adc57c7151294
Parth Aggarwal <parth21999@gmail.com>
2020-06-12 12:07:30 -0400

    added win32 reals

---
+++ win32/tests/reals_win32/real_threadapi.c
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#define GBALLOC_H
+
+#include "real_threadapi_renames.h"
+
+#include "threadapi_win32.c"
+
