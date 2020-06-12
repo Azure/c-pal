7eec538e3c60db73ef5f059520dfd0794cbc7bef
parth21999 <parth21999@gmail.com>
2020-06-12 15:51:05 -0400

    added reals_linux, ignored vscode

---
+++ linux/tests/reals_linux_ut/reals_linux_ut.c
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "testrunnerswitcher.h"
+
+#define REGISTER_GLOBAL_MOCK_HOOK(original, real) \
+    (original == real) ? (void)0 : (void)1;
+
+#include "threadapi.h"
+
+#include "real_threadapi.h"
+
+BEGIN_TEST_SUITE(reals_linux_ut)
+
+// this test makes sure that the mappings work
+// (there is a real_ function corresponding to the original)
+TEST_FUNCTION(check_all_c_pal_reals)
+{
+    // arrange
+
+    // act
+    REGISTER_THREADAPI_GLOBAL_MOCK_HOOK();
+
+    // assert
+    // no explicit assert, if it builds it works
+}
+
+END_TEST_SUITE(reals_linux_ut)
