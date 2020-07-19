cef219bc1803aa9a51562658cbe92b4f3b6f0fdd
Andrei Porumb <anporumb@microsoft.com>
2020-07-19 10:58:23 -0700

    save before new computer

--- common/inc/azure_c_pal/gballoc_hl.h
+++ common/inc/azure_c_pal/gballoc_hl.h
@@ -17,12 +17,12 @@ extern "C"
 {
 #endif
 
-    MOCKABLE_FUNCTION(, gballoc_hl_init, void*, params);
-    MOCKABLE_FUNCTION(, gballoc_hl_deinit);
+    MOCKABLE_FUNCTION(, int, gballoc_hl_init, void*, gballoc_hl_init_params, void*, gballoc_ll_init_params);
+    MOCKABLE_FUNCTION(, void, gballoc_hl_deinit);
 
     MOCKABLE_FUNCTION(, void*, gballoc_hl_malloc, size_t, size);
     MOCKABLE_FUNCTION(, void, gballoc_hl_free, void*, ptr);
-    MOCKABLE_FUNCTION(, void, gballoc_hl_calloc, size_t, nmemb, size_t, size);
+    MOCKABLE_FUNCTION(, void*, gballoc_hl_calloc, size_t, nmemb, size_t, size);
     MOCKABLE_FUNCTION(, void*, gballoc_hl_realloc, void*, ptr, size_t, size);
 
 #ifdef __cplusplus
