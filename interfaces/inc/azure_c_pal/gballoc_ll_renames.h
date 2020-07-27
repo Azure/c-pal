0053ba1fbff3132c82ce80e63dc3c357b4d0ed00
Andrei Porumb <anporumb@microsoft.com>
2020-07-27 07:46:35 -0700

    timer does not use gballoc (vanilla) or gballoc_hl

---
+++ interfaces/inc/azure_c_pal/gballoc_ll_renames.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef GBALLOC_LL_RENAMES_H
+#define GBALLOC_LL_RENAMES_H
+
+/*this file provides a convenient way of having all the malloc/free preprocessor tokens in a source file replaced by their gballoc_ll counterparts*/
+
+#define malloc gballoc_ll_malloc
+#define free gballoc_ll_free
+#define calloc gballoc_ll_calloc
+#define realloc gballoc_ll_realloc
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GBALLOC_LL_RENAMES_H */
