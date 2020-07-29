3a25d2524a7aee53bdbf41f5e0520cbcf794c7c1
Andrei Porumb <anporumb@microsoft.com>
2020-07-28 19:32:05 -0700

    hm nu merge

--- win32/reals/real_interlocked_hl_win32.c
+++ win32/reals/real_interlocked_hl_win32.c
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-#define GBALLOC_H
-
 #include "real_interlocked_hl_renames.h"
 
 #include "interlocked_hl_win32.c"
--- win32/reals/real_threadapi.c
+++ win32/reals/real_threadapi.c
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define GBALLOC_H
-
 #include "real_threadapi_renames.h"
 
 #include "threadapi_win32.c"
