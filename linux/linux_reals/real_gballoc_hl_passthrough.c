e779a8646467ef6a0c879b13123dc7190690c076
Andrei Porumb <anporumb@microsoft.com>
2020-07-30 10:59:12 -0700

    add real gballoc

---
+++ linux/linux_reals/real_gballoc_hl_passthrough.c
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "real_gballoc_ll_renames.h"
+
+#include "real_gballoc_hl_renames.h"
+
+#include "../src/gballoc_hl_passthrough.c"
+
