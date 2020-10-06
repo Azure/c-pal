008343386475ccd2560fa8184da900b45ce3c1dd
Dan Cristoloveanu <dcristo@microsoft.com>
2020-10-06 14:17:53 -0700

    Remove interlocked_hl

--- win32/reals/real_call_once.c
+++ win32/reals/real_call_once.c
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "real_interlocked_renames.h"
-#include "real_interlocked_hl_renames.h"
 #include "real_sync_renames.h"
 
 #include "real_call_once_renames.h"
