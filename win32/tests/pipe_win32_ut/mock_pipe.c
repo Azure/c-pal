3d1fe179f81d0f8e64c8f46ec2336e375f7f3cc2
Matt Durak <mdurak@microsoft.com>
2021-01-21 14:24:45 -0800

    Fix build

--- win32/tests/pipe_win32_ut/mock_pipe.c
+++ win32/tests/pipe_win32_ut/mock_pipe.c
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <errno.h>
 #include "mock_pipe.h"
 
 #undef _popen
