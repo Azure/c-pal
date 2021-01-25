c36fab1a431e98d038ae501aebb25e9efd72f7a3
Matt Durak <mdurak@microsoft.com>
2021-01-25 08:57:29 -0800

    Fix iwyu being overly aggressive and failing linux ut

--- linux/tests/sync_linux_ut/mock_sync.c
+++ linux/tests/sync_linux_ut/mock_sync.c
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include <errno.h> // IWYU pragma: keep
+
 #undef syscall 
 #define syscall mock_syscall
 #undef errno
