6b1816cacffc8e186e2f5c0e4de918f2d14ac6d1
Andrei Porumb <anporumb@microsoft.com>
2021-10-12 13:39:35 -0700

    temp save

--- win32/src/gballoc_ll_mimalloc.c
+++ win32/src/gballoc_ll_mimalloc.c
@@ -3,11 +3,13 @@
 
 #include <stddef.h>
 #include <stdlib.h>
-
-#include "c_logging/xlogging.h"
-#include "c_pal/gballoc_ll.h"
+#include <stdint.h>
 
 #include "mimalloc.h"
+
+#include "c_logging/xlogging.h"
+
+#include "c_pal/gballoc_ll.h"
 
 int gballoc_ll_init(void* params)
 {
@@ -21,16 +23,61 @@ void gballoc_ll_deinit(void)
     /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_002: [ gballoc_ll_deinit shall return. ] */
 }
 
-void* gballoc_ll_malloc(size_t size)
+static void* gballoc_ll_malloc_internal(size_t size)
 {
     void* result;
+
     /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_003: [ gballoc_ll_malloc shall call mi_malloc and returns what mi_malloc returned. ]*/
-    if ((result = mi_malloc(size)) == NULL)
+    result = mi_malloc(size);
+
+    if (result == NULL)
     {
         LogError("failure in mi_malloc(size=%zu)", size);
     }
 
     return result;
+}
+
+void* gballoc_ll_malloc(size_t size)
+{
+    return gballoc_ll_malloc_internal(size);
+}
+
+void* gballoc_ll_malloc_2(size_t nmemb, size_t size)
+{
+    void* result;
+
+    /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_008: [ If nmemb * size exceeds SIZE_MAX then gballoc_ll_malloc_2 shall fail and return NULL. ]*/
+    if (SIZE_MAX / nmemb < size)
+    {
+        LogError("overflow in computation of nmemb=%zu * size=%zu",
+            nmemb, size);
+        result = NULL;
+    }
+    else
+    {
+        /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_009: [ gballoc_ll_malloc_2 shall call mi_malloc(nmemb * size) and returns what mi_malloc returned. ]*/
+        result = gballoc_ll_malloc_internal(nmemb * size);
+    }
+    return result;
+}
+
+void* gballoc_ll_malloc_flex(size_t base, size_t nmemb, size_t size)
+{
+    /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_010: [ If nmemb * size exceeds SIZE_MAX then gballoc_ll_malloc_flex shall fail and return NULL. ]*/
+    if (SIZE_MAX / nmemb < size)
+    {
+        LogError("overflow in computation of nmemb=%zu * size=%zu",
+            nmemb, size);
+        result = NULL;
+    }
+    else
+    {
+        /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_011: [ If base + nmemb * size exceeds SIZE_MAX then gballoc_ll_malloc_flex shall fail and return NULL. ]*/
+
+        /*Codes_SRS_GBALLOC_LL_MIMALLOC_02_009: [ gballoc_ll_malloc_2 shall call mi_malloc(nmemb * size) and returns what mi_malloc returned. ]*/
+        result = gballoc_ll_malloc_internal(nmemb * size);
+    }
 }
 
 void gballoc_ll_free(void* ptr)
