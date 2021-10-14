e640e41815f5e26e7c4a2bbdd8261d9797c63cdc
Andrei Porumb <anporumb@microsoft.com>
2021-10-14 09:32:38 -0700

    have a zero implementation

--- linux/src/arithmetic_linux.c
+++ linux/src/arithmetic_linux.c
@@ -1,14 +1,25 @@
 // Copyright (C) Microsoft Corporation. All rights reserved.
 
+#include <stdint.h>
+
 #include "c_pal/arithmetic.h"
 
-/*only works if compiling on 64 bits - for now*/
+#if INTPTR_MAX > 0x7FFFFFFFUL
+/*only works if compiling on 64 bits*/
 PAL_UINT128 umul64x64(uint64_t left, uint64_t right)
 {
     /*Codes_SRS_ARITHMETIC_02_001: [ umul64x64 shall call _umul128 and return the result as PAL_UINT128. ]*/
     PAL_UINT128 result;
-    unsigned __int128 temp = (unsigned __int128)left * right;
-    result.low = temp & 0xFFFFFFFFFFFFFFFF;
-    result.high = temp >> 64;
+    result.low = _umul128(left, right, &result.high);
     return result;
 }
+#else
+
+static PAL_UINT128 zer0 = { 0 };
+
+PAL_UINT128 umul64x64(uint64_t left, uint64_t right)
+{
+    return zer0;
+}
+
+#endif
--- win32/src/arithmetic_win32.c
+++ win32/src/arithmetic_win32.c
@@ -1,10 +1,13 @@
 // Copyright (C) Microsoft Corporation. All rights reserved.
 
+#include <stdint.h>
+
 #include <intrin.h>
 
 #include "c_pal/arithmetic.h"
 
-/*only works if compiling on 64 bits - for now*/
+#if INTPTR_MAX > 0x7FFFFFFFUL
+/*only works if compiling on 64 bits*/
 PAL_UINT128 umul64x64(uint64_t left, uint64_t right)
 {
     /*Codes_SRS_ARITHMETIC_02_001: [ umul64x64 shall call _umul128 and return the result as PAL_UINT128. ]*/
@@ -12,3 +15,14 @@ PAL_UINT128 umul64x64(uint64_t left, uint64_t right)
     result.low = _umul128(left, right, &result.high);
     return result;
 }
+#else
+
+static PAL_UINT128 zer0 = { 0 };
+
+PAL_UINT128 umul64x64(uint64_t left, uint64_t right)
+{
+    return zer0;
+}
+
+#endif
+
