7405d6761c1304ec7afe63bd13d5b013dfbd30de
Andrei Porumb <anporumb@microsoft.com>
2022-01-10 07:44:02 -0800

    include what you use should stop complaining

--- umocktypes/tests/umocktypes_uuid_t_int/umocktypes_uuid_t_int.c
+++ umocktypes/tests/umocktypes_uuid_t_int/umocktypes_uuid_t_int.c
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdlib.h>
-#include <stdint.h>
-
 #include "macro_utils/macro_utils.h" // IWYU pragma: keep
 #include "testrunnerswitcher.h"
 
