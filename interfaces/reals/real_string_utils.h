ccdf7b3cfcbfae28a352d6ba200f9257f6334fc0
anporumb <anporumb@users.noreply.github.com>
2022-08-11 01:59:05 -0700

    add linux string reals (#162)
    * have string reals

--- interfaces/reals/real_string_utils.h
+++ interfaces/reals/real_string_utils.h
@@ -10,12 +10,15 @@
 #include <stdbool.h>
 #endif
 
+#ifdef WIN32
 #include "windows.h"
+#endif
 
 #include "macro_utils/macro_utils.h"
 
 #define R2(X) REGISTER_GLOBAL_MOCK_HOOK(X, real_##X);
 
+#ifdef WIN32
 #define REGISTER_STRING_UTILS_GLOBAL_MOCK_HOOK() \
     MU_FOR_EACH_1(R2, \
         vsprintf_char, \
@@ -25,6 +28,15 @@
         mbs_to_wcs, \
         wcs_to_mbs \
 )
+#else
+    #define REGISTER_STRING_UTILS_GLOBAL_MOCK_HOOK() \
+    MU_FOR_EACH_1(R2, \
+        vsprintf_char, \
+        vsprintf_wchar, \
+        mbs_to_wcs, \
+        wcs_to_mbs \
+)
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,9 +50,11 @@ char* real_vsprintf_char(const char* format, va_list va);
 
 wchar_t* real_vsprintf_wchar(const wchar_t* format, va_list va);
 
+#ifdef WIN32
 char* real_FILETIME_toAsciiArray(const FILETIME* fileTime);
 
 char* real_FILETIME_to_string_UTC(const FILETIME* fileTime);
+#endif
 
 wchar_t* real_mbs_to_wcs(const char* source);
 
--- linux/tests/linux_reals_ut/reals_linux_ut.c
+++ linux/tests/linux_reals_ut/reals_linux_ut.c
@@ -5,6 +5,7 @@
 
 #include "c_pal/arithmetic.h" // IWYU pragma: keep
 #include "c_pal/uuid.h" // IWYU pragma: keep
+#include "c_pal/string_utils.h" // IWYU pragma: keep
 
 #define REGISTER_GLOBAL_MOCK_HOOK(original, real) \
     (original == real) ? (void)0 : (void)1;
@@ -13,6 +14,7 @@
 #include "real_sync.h"
 #include "real_arithmetic.h"
 #include "real_uuid.h"
+#include "real_string_utils.h"
 
 BEGIN_TEST_SUITE(TEST_SUITE_NAME_FROM_CMAKE)
 
@@ -27,6 +29,7 @@ TEST_FUNCTION(check_all_c_pal_reals)
     REGISTER_SYNC_GLOBAL_MOCK_HOOK();
     REGISTER_ARITHMETIC_GLOBAL_MOCK_HOOK();
     REGISTER_UUID_GLOBAL_MOCK_HOOK();
+    REGISTER_STRING_UTILS_GLOBAL_MOCK_HOOK();
 
     // assert
     // no explicit assert, if it builds it works
