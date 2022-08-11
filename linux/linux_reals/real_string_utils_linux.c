bdf689981afd917035fbd892d719c560e776ab14
Andrei Porumb <anporumb@microsoft.com>
2022-08-11 01:28:23 -0700

    have string reals

---
+++ linux/linux_reals/real_string_utils_linux.c
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "real_gballoc_hl_renames.h" // IWYU pragma: keep
+
+#include "real_string_utils_renames.h" // IWYU pragma: keep
+
+#include "../src/string_utils.c"
--- linux/tests/linux_reals_ut/reals_linux_ut.c
+++ linux/tests/linux_reals_ut/reals_linux_ut.c
@@ -13,6 +13,9 @@
 #include "real_sync.h"
 #include "real_arithmetic.h"
 #include "real_uuid.h"
+#include "real_string_utils.h"
+
+#include "c_pal/string_utils.h"
 
 BEGIN_TEST_SUITE(TEST_SUITE_NAME_FROM_CMAKE)
 
@@ -27,6 +30,7 @@ TEST_FUNCTION(check_all_c_pal_reals)
     REGISTER_SYNC_GLOBAL_MOCK_HOOK();
     REGISTER_ARITHMETIC_GLOBAL_MOCK_HOOK();
     REGISTER_UUID_GLOBAL_MOCK_HOOK();
+    REGISTER_STRING_UTILS_GLOBAL_MOCK_HOOK();
 
     // assert
     // no explicit assert, if it builds it works
