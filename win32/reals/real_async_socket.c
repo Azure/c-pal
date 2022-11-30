39351a7d71249aa332cc36fc6cf425da60eaec0f
Andrei Porumb <anporumb@microsoft.com>
2022-11-30 10:22:06 -0800

    execution_engine_win32 maybe 2

--- win32/reals/real_async_socket.c
+++ win32/reals/real_async_socket.c
@@ -3,6 +3,12 @@
 
 #include "real_gballoc_hl_renames.h"
 
+#include "real_threadpool_renames.h"
+
+#include "real_execution_engine_renames.h"
+
+#include "real_execution_engine_win32_renames.h"
+
 #include "real_async_socket_renames.h" // IWYU pragma: keep
 
 #include "../src/async_socket_win32.c"
