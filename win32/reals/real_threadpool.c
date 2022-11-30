49f439a49088b9b68a0ede75f91cfa36144aa769
Andrei Porumb <anporumb@microsoft.com>
2022-11-30 09:57:00 -0800

    maybe threadpool 2

--- win32/reals/real_threadpool.c
+++ win32/reals/real_threadpool.c
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include "real_gballoc_hl_renames.h"
+
 #include "real_threadpool_renames.h" // IWYU pragma: keep
 
 #include "../src/threadpool_win32.c"
