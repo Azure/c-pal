1c2d98cd23e5208684d0ba95bec81b58839ecc36
Andrei Porumb <anporumb@microsoft.com>
2022-11-30 10:23:12 -0800

    execution_engine_win32 maybe 3

--- win32/reals/real_threadpool.c
+++ win32/reals/real_threadpool.c
@@ -3,6 +3,10 @@
 
 #include "real_gballoc_hl_renames.h"
 
+#include "real_execution_engine_renames.h"
+
+#include "real_execution_engine_win32_renames.h"
+
 #include "real_threadpool_renames.h" // IWYU pragma: keep
 
 #include "../src/threadpool_win32.c"
