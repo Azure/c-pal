5232411f9fffefa927014378ffecaaf556748e77
Dan Cristoloveanu <dcristo@microsoft.com>
2022-12-05 20:22:06 -0800

    Remove unnecessary casts (#186)

--- win32/tests/gballoc_ll_jemalloc_int/gballoc_ll_jemalloc_int.c
+++ win32/tests/gballoc_ll_jemalloc_int/gballoc_ll_jemalloc_int.c
@@ -61,7 +61,7 @@ TEST_FUNCTION(gballoc_ll_deinit_works)
 TEST_FUNCTION(gballoc_ll_malloc_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc(1);
+    unsigned char* ptr = gballoc_ll_malloc(1);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -78,7 +78,7 @@ TEST_FUNCTION(gballoc_ll_malloc_works)
 TEST_FUNCTION(gballoc_ll_malloc_1MB_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc(1024 * 1024);
+    unsigned char* ptr = gballoc_ll_malloc(1024 * 1024);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -95,7 +95,7 @@ TEST_FUNCTION(gballoc_ll_malloc_1MB_works)
 TEST_FUNCTION(gballoc_ll_malloc_2_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc_2(1, 1);
+    unsigned char* ptr = gballoc_ll_malloc_2(1, 1);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -112,7 +112,7 @@ TEST_FUNCTION(gballoc_ll_malloc_2_works)
 TEST_FUNCTION(gballoc_ll_malloc_2_1MB_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc_2(1024, 1024);
+    unsigned char* ptr = gballoc_ll_malloc_2(1024, 1024);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -129,7 +129,7 @@ TEST_FUNCTION(gballoc_ll_malloc_2_1MB_works)
 TEST_FUNCTION(gballoc_ll_malloc_flex_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc_flex(1, 1, 1);
+    unsigned char* ptr = gballoc_ll_malloc_flex(1, 1, 1);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -146,7 +146,7 @@ TEST_FUNCTION(gballoc_ll_malloc_flex_works)
 TEST_FUNCTION(gballoc_ll_malloc_flex_1MB_works)
 {
     ///act (1)
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc_flex(1024, 1024, 1024);
+    unsigned char* ptr = gballoc_ll_malloc_flex(1024, 1024, 1024);
 
     ///assert (1)
     ASSERT_IS_NOT_NULL(ptr);
@@ -163,7 +163,7 @@ TEST_FUNCTION(gballoc_ll_malloc_flex_1MB_works)
 TEST_FUNCTION(gballoc_ll_free_works)
 {
     ///arrange
-    unsigned char* ptr = (unsigned char*)gballoc_ll_malloc(1);
+    unsigned char* ptr = gballoc_ll_malloc(1);
     ASSERT_IS_NOT_NULL(ptr);
 
     ///act 
@@ -175,12 +175,12 @@ TEST_FUNCTION(gballoc_ll_free_works)
 TEST_FUNCTION(gballoc_ll_realloc_works)
 {
     ///arrange
-    unsigned char* ptr1 = (unsigned char*)gballoc_ll_malloc(1);
+    unsigned char* ptr1 = gballoc_ll_malloc(1);
     ASSERT_IS_NOT_NULL(ptr1);
     unsigned char* ptr2;
 
     ///act 
-    ptr2 = (unsigned char*)gballoc_ll_realloc(ptr1, 2);
+    ptr2 = gballoc_ll_realloc(ptr1, 2);
 
     ///assert - doesn't crash
     ASSERT_IS_NOT_NULL(ptr2);
@@ -192,12 +192,12 @@ TEST_FUNCTION(gballoc_ll_realloc_works)
 TEST_FUNCTION(gballoc_ll_realloc_2_works)
 {
     ///arrange
-    unsigned char* ptr1 = (unsigned char*)gballoc_ll_malloc(1);
+    unsigned char* ptr1 = gballoc_ll_malloc(1);
     ASSERT_IS_NOT_NULL(ptr1);
     unsigned char* ptr2;
 
     ///act 
-    ptr2 = (unsigned char*)gballoc_ll_realloc_2(ptr1, 1, 2);
+    ptr2 = gballoc_ll_realloc_2(ptr1, 1, 2);
 
     ///assert - doesn't crash
     ASSERT_IS_NOT_NULL(ptr2);
@@ -209,12 +209,12 @@ TEST_FUNCTION(gballoc_ll_realloc_2_works)
 TEST_FUNCTION(gballoc_ll_realloc_flex_works)
 {
     ///arrange
-    unsigned char* ptr1 = (unsigned char*)gballoc_ll_malloc_flex(4, 10, 8);
+    unsigned char* ptr1 = gballoc_ll_malloc_flex(4, 10, 8);
     ASSERT_IS_NOT_NULL(ptr1);
     unsigned char* ptr2;
 
     ///act 
-    ptr2 = (unsigned char*)gballoc_ll_realloc_flex(ptr1, 4, 20, 8);
+    ptr2 = gballoc_ll_realloc_flex(ptr1, 4, 20, 8);
 
     ///assert - doesn't crash
     ASSERT_IS_NOT_NULL(ptr2);
@@ -230,7 +230,7 @@ TEST_FUNCTION(gballoc_ll_calloc_works)
     unsigned char* ptr;
 
     ///act 
-    ptr = (unsigned char*)gballoc_ll_calloc(1, 1);
+    ptr = gballoc_ll_calloc(1, 1);
 
     ///assert - doesn't crash
     ASSERT_IS_NOT_NULL(ptr);
