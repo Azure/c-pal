0f80fadbb91c13f01348527ee45e903b0b4724f8
Jelani Brandon <jebrando@microsoft.com>
2022-12-23 20:18:40 -0800

    listen fail

--- linux/tests/async_socket_linux_int/async_socket_linux_int.c
+++ linux/tests/async_socket_linux_int/async_socket_linux_int.c
@@ -127,7 +127,7 @@ static void setup_server_socket(SOCKET_HANDLE* listen_socket)
     set_nonblocking(*listen_socket);
 
     // start listening
-    ASSERT_ARE_EQUAL(int, 0, listen(*listen_socket, SOMAXCONN), "Failure on listen socket error (%d): %s", errno, strerror(errno));
+    ASSERT_ARE_EQUAL(int, 0, listen(*listen_socket, SOMAXCONN), "Failure on listen socket error (%d): port: %d", errno, g_port_num);
 }
 
 static void setup_test_socket(int port_num, SOCKET_HANDLE* client_socket, SOCKET_HANDLE* listen_socket, SOCKET_HANDLE* accept_socket)
