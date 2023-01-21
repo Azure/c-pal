451623794d2b3f0741771ee4e3066f3ddbadafae
Bingyi Yang <bingyiyang@microsoft.com>
2023-01-20 17:36:53 -0800

    fix some compile errors

--- common/tests/sm_ut/sm_ut.c
+++ common/tests/sm_ut/sm_ut.c
@@ -15,7 +15,7 @@
 #include "umock_c/umock_c.h"
 #include "umock_c/umocktypes_stdint.h"
 #include "umock_c/umocktypes.h"
-
+// IWYU pragma: no_include "c_pal/ps_util.h" 
 #include "c_pal/interlocked.h"// IWYU pragma: keep
 
 #define ENABLE_MOCKS
