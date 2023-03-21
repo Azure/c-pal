0d59f8e97fe6dd969ef44eab1f7216735b2b5eed
Bingyi Yang <bingyiyang@microsoft.com>
2023-03-21 16:33:11 -0700

    add execution spec and ut

--- linux/devdoc/execution_engine_linux_requirement.md
+++ linux/devdoc/execution_engine_linux_requirement.md
@@ -37,17 +37,17 @@ MOCKABLE_FUNCTION(, EXECUTION_ENGINE_HANDLE, execution_engine_create, void*, exe
 
 `execution_engine_create` creates an execution engine.
 
-`execution_engine_create` shall allocate a new execution engine and on success shall return a non-NULL handle.
+**SRS_EXECUTION_ENGINE_LINUX_07_001: [** `execution_engine_create` shall allocate a new execution engine and on success shall return a non-NULL handle. **]**
 
-If `execution_engine_parameters` is NULL, `execution_engine_create` shall use the default `DEFAULT_MIN_THREAD_COUNT` and `DEFAULT_MAX_THREAD_COUNT` as parameters.
+**SRS_EXECUTION_ENGINE_LINUX_07_002: [** If `execution_engine_parameters` is NULL, `execution_engine_create` shall use the default `DEFAULT_MIN_THREAD_COUNT` and `DEFAULT_MAX_THREAD_COUNT` as parameters. **]**
 
-`execution_engine_create` shall set the minimum number of threads to the `min_thread_count` field of `execution_engine_parameters`.
+**SRS_EXECUTION_ENGINE_LINUX_07_003: [** `execution_engine_create` shall set the minimum number of threads to the `min_thread_count` field of `execution_engine_parameters`. **]**
 
-`execution_engine_create` shall set the maximum number of threads to the `max_thread_count` field of `execution_engine_parameters`.
+**SRS_EXECUTION_ENGINE_LINUX_07_004: [** `execution_engine_create` shall set the maximum number of threads to the `max_thread_count` field of `execution_engine_parameters`. **]**
 
-If `max_thread_count` is non-zero and less than `min_thread_count`, `execution_engine_create` shall fail and return NULL.
+**SRS_EXECUTION_ENGINE_LINUX_07_005: [** If `max_thread_count` is non-zero and less than `min_thread_count`, `execution_engine_create` shall fail and return NULL. **]**
 
-If any error occurs, `execution_engine_create` shall fail and return NULL.
+**SRS_EXECUTION_ENGINE_LINUX_07_006: [** If any error occurs, `execution_engine_create` shall fail and return NULL. **]**
 
 ### execution_engine_dec_ref
 
@@ -57,11 +57,11 @@ MOCKABLE_FUNCTION(, void, execution_engine_dec_ref, EXECUTION_ENGINE_HANDLE, exe
 
 `execution_engine_dec_ref` decrements the ref count and frees all resources associated with `execution_engine` if needed.
 
-If `execution_engine` is NULL, `execution_engine_dec_ref` shall return.
+**SRS_EXECUTION_ENGINE_LINUX_07_007: [** If `execution_engine` is NULL, `execution_engine_dec_ref` shall return. **]**
 
-Otherwise `execution_engine_dec_ref` shall decrement the refcount.
+**SRS_EXECUTION_ENGINE_LINUX_07_008: [** Otherwise `execution_engine_dec_ref` shall decrement the refcount. **]**
 
-If the refcount is zero `execution_engine_dec_ref` shall free the memory for `EXECUTION_ENGINE`.
+**SRS_EXECUTION_ENGINE_LINUX_07_009: [** If the refcount is zero `execution_engine_dec_ref` shall free the memory for `EXECUTION_ENGINE`. **]**
 
 ### execution_engine_inc_ref
 
@@ -71,9 +71,9 @@ MOCKABLE_FUNCTION(, void, execution_engine_inc_ref, EXECUTION_ENGINE_HANDLE, exe
 
 `execution_engine_inc_ref` increments the ref count on the `execution_engine`.
 
-If `execution_engine` is `NULL` then `execution_engine_inc_ref` shall return.
+**SRS_EXECUTION_ENGINE_LINUX_07_010: [** If `execution_engine` is `NULL` then `execution_engine_inc_ref` shall return. **]**
 
-Otherwise `execution_engine_inc_ref` shall increment the reference count for `execution_engine`.
+**SRS_EXECUTION_ENGINE_LINUX_07_011: [** Otherwise `execution_engine_inc_ref` shall increment the reference count for `execution_engine`. **]**
 
 
 ### execution_engine_linux_get_parameters
@@ -84,6 +84,6 @@ MOCKABLE_FUNCTION(, const EXECUTION_ENGINE_PARAMETERS_LINUX*, execution_engine_l
 
 `execution_engine_linux_get_parameters` returns the `EXECUTION_ENGINE_PARAMETERS_LINUX` handle.
 
-If `execution_engine` is NULL, `execution_engine_linux_get_parameters` shall fail and return NULL.
+**SRS_EXECUTION_ENGINE_LINUX_07_012: [** If `execution_engine` is NULL, `execution_engine_linux_get_parameters` shall fail and return NULL. **]**
 
-Otherwise, `execution_engine_linux_get_parameters` shall return the parameters in `EXECUTION_ENGINE`.
+**SRS_EXECUTION_ENGINE_LINUX_07_013: [** Otherwise, `execution_engine_linux_get_parameters` shall return the parameters in `EXECUTION_ENGINE`. **]**
--- linux/src/execution_engine_linux.c
+++ linux/src/execution_engine_linux.c
@@ -22,20 +22,25 @@ EXECUTION_ENGINE_HANDLE execution_engine_create(void* execution_engine_parameter
 {
     EXECUTION_ENGINE_HANDLE result;
 
+    /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_001: [ execution_engine_create shall allocate a new execution engine and on success shall return a non-NULL handle. ]*/
     result = REFCOUNT_TYPE_CREATE(EXECUTION_ENGINE);
     if (result == NULL)
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_006: [ If any error occurs, execution_engine_create shall fail and return NULL. ]*/
         LogError("REFCOUNT_TYPE_CREATE failed.");
     }
     else
     {
         if (execution_engine_parameters == NULL)
         {
+            /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_002: [ If execution_engine_parameters is NULL, execution_engine_create shall use the default DEFAULT_MIN_THREAD_COUNT and DEFAULT_MAX_THREAD_COUNT as parameters. ]*/
             result->params.min_thread_count = DEFAULT_MIN_THREAD_COUNT;
             result->params.max_thread_count = DEFAULT_MAX_THREAD_COUNT;
         }
         else
         {
+            /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_003: [ execution_engine_create shall set the minimum number of threads to the min_thread_count field of execution_engine_parameters. ]*/
+            /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_004: [ execution_engine_create shall set the maximum number of threads to the max_thread_count field of execution_engine_parameters. ]*/
             EXECUTION_ENGINE_PARAMETERS_LINUX* parameters_linux = (EXECUTION_ENGINE_PARAMETERS_LINUX*)execution_engine_parameters;
 
             result->params.min_thread_count = parameters_linux->min_thread_count;
@@ -45,6 +50,7 @@ EXECUTION_ENGINE_HANDLE execution_engine_create(void* execution_engine_parameter
         if ((result->params.max_thread_count != 0) &&
             (result->params.max_thread_count < result->params.min_thread_count))
         {
+            /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_005: [ If max_thread_count is non-zero and less than min_thread_count, execution_engine_create shall fail and return NULL. ]*/
             LogError("Invalid arguments: execution_engine_parameters_win32->min_thread_count=%" PRIu32 ", execution_engine_parameters_win32->max_thread_count=%" PRIu32,
                 result->params.min_thread_count, result->params.max_thread_count);
         }
@@ -65,12 +71,16 @@ void execution_engine_dec_ref(EXECUTION_ENGINE_HANDLE execution_engine)
 {
     if (execution_engine == NULL)
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_007: [ If execution_engine is NULL, execution_engine_dec_ref shall return. ]*/
         LogError("Invalid arguments: EXECUTION_ENGINE_HANDLE execution_engine=%p", execution_engine);
     }
     else
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_008: [ Otherwise execution_engine_dec_ref shall decrement the refcount. ]*/
         if (DEC_REF(EXECUTION_ENGINE, execution_engine) == 0)
         {
+
+            /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_009: [ If the refcount is zero execution_engine_dec_ref shall free the memory for EXECUTION_ENGINE. ]*/
             REFCOUNT_TYPE_DESTROY(EXECUTION_ENGINE, execution_engine);
         }
     }
@@ -80,10 +90,12 @@ void execution_engine_inc_ref(EXECUTION_ENGINE_HANDLE execution_engine)
 {
     if (execution_engine == NULL)
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_010: [ If execution_engine is NULL then execution_engine_inc_ref shall return. ]*/
         LogError("Invalid arguments: EXECUTION_ENGINE_HANDLE execution_engine=%p", execution_engine);
     }
     else
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_011: [ Otherwise execution_engine_inc_ref shall increment the reference count for execution_engine. ]*/
         INC_REF(EXECUTION_ENGINE, execution_engine);
     }
 }
@@ -94,11 +106,13 @@ const EXECUTION_ENGINE_PARAMETERS_LINUX* execution_engine_linux_get_parameters(E
 
     if (execution_engine == NULL)
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_012: [ If execution_engine is NULL, execution_engine_linux_get_parameters shall fail and return NULL. ]*/
         LogError("Invalid arguments: EXECUTION_ENGINE_HANDLE execution_engine=%p", execution_engine);
         result = NULL;
     }
     else
     {
+        /* Codes_SRS_EXECUTION_ENGINE_LINUX_07_013: [ Otherwise, execution_engine_linux_get_parameters shall return the parameters in EXECUTION_ENGINE. ]*/
         result = &execution_engine->params;
     }
 
