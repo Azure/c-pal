be1f18a92657993e0904c1fd2cb877ee38e82b4d
Jelani Brandon <jebrando@microsoft.com>
2023-04-03 13:35:10 -0700

    update more includes

--- common/tests/thandle_ut/thandle_user_33_characters.c
+++ common/tests/thandle_ut/thandle_user_33_characters.c
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdlib.h>
-#include <limits.h>
 
 #include "macro_utils/macro_utils.h"
 
