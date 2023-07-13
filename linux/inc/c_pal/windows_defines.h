6da0f993265910f4be52fa0d7df4d4229e8a712c
samrach9 <97257043+samrach9@users.noreply.github.com>
2023-07-13 14:07:23 -0700

    Source and test files for file_util_linux (#254)

--- linux/inc/c_pal/windows_defines.h
+++ linux/inc/c_pal/windows_defines.h
@@ -15,21 +15,21 @@ extern "C" {
 typedef void *HANDLE;
 typedef void *LPSECURITY_ATTRIBUTES;
 
-#define GENERIC_READ                    0x80000000
-#define GENERIC_WRITE                   0x40000000
-#define GENERIC_ALL                     0x80000000 | 0x40000000
+#define GENERIC_READ                    (0x80000000)
+#define GENERIC_WRITE                   (0x40000000)
+#define GENERIC_ALL                     (0x80000000 | 0x40000000)
 
-#define FILE_SHARE_READ                 0x00000001
-#define FILE_SHARE_WRITE                0x00000002
-#define FILE_SHARE_DELETE               0x00000004
+#define FILE_SHARE_READ                 (0x00000001)
+#define FILE_SHARE_WRITE                (0x00000002)
+#define FILE_SHARE_DELETE               (0x00000004)
 
-#define FILE_FLAG_WRITE_THROUGH         0x80000000
-#define FILE_FLAG_OVERLAPPED            0x40000000
-#define FILE_FLAG_NO_BUFFERING          0x20000000
-#define FILE_FLAG_RANDOM_ACCESS         0x10000000
-#define FILE_FLAG_SEQUENTIAL_SCAN       0x08000000
-#define FILE_FLAG_DELETE_ON_CLOSE       0x04000000
-#define FILE_FLAG_OPEN_NO_RECALL        0x00100000
+#define FILE_FLAG_WRITE_THROUGH         (0x80000000)
+#define FILE_FLAG_OVERLAPPED            (0x40000000)
+#define FILE_FLAG_NO_BUFFERING          (0x20000000)
+#define FILE_FLAG_RANDOM_ACCESS         (0x10000000)
+#define FILE_FLAG_SEQUENTIAL_SCAN       (0x08000000)
+#define FILE_FLAG_DELETE_ON_CLOSE       (0x04000000)
+#define FILE_FLAG_OPEN_NO_RECALL        (0x00100000)
 
 #define CREATE_NEW                      1
 #define CREATE_ALWAYS                   2
