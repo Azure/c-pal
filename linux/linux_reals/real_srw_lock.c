a67e0009fcf0fc309df08fa27cbe0b70cef5f2ad
Parth Aggarwal <paaggarwal@microsoft.com>
2023-07-31 14:24:35 -0700

    Fix reals for srw linux (#260)

--- linux/linux_reals/real_srw_lock.c
+++ linux/linux_reals/real_srw_lock.c
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#include "real_gballoc_hl_renames.h"  // IWYU pragma: keep
+#include "real_string_utils_renames.h" // IWYU pragma: keep
 
 #include "real_srw_lock_renames.h" // IWYU pragma: keep
 
--- linux/tests/linux_reals_ut/reals_linux_ut.c
+++ linux/tests/linux_reals_ut/reals_linux_ut.c
@@ -4,6 +4,7 @@
 #include "testrunnerswitcher.h"
 
 #include "c_pal/arithmetic.h" // IWYU pragma: keep
+#include "c_pal/srw_lock.h" // IWYU pragma: keep
 #include "c_pal/string_utils.h" // IWYU pragma: keep
 #include "c_pal/threadpool.h" // IWYU pragma: keep
 #include "c_pal/uuid.h" // IWYU pragma: keep
@@ -12,6 +13,7 @@
     (original == real) ? (void)0 : (void)1;
 
 #include "real_arithmetic.h"
+#include "real_srw_lock.h"
 #include "real_string_utils.h"
 #include "real_sync.h"
 #include "real_threadapi.h"
@@ -28,6 +30,7 @@ TEST_FUNCTION(check_all_c_pal_reals)
 
     // act
     REGISTER_ARITHMETIC_GLOBAL_MOCK_HOOK();
+    REGISTER_SRW_LOCK_GLOBAL_MOCK_HOOK();
     REGISTER_STRING_UTILS_GLOBAL_MOCK_HOOK();
     REGISTER_SYNC_GLOBAL_MOCK_HOOK();
     REGISTER_THREADAPI_GLOBAL_MOCK_HOOK();
