b182f1f81223f5f5e92ffe04d46b3de18023d004
Dan Cristoloveanu <dcristo@microsoft.com>
2023-08-31 19:57:30 -0700

    Add reals for srw_lock_ll

--- interfaces/reals/real_srw_lock_ll.h
+++ interfaces/reals/real_srw_lock_ll.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#ifndef REAL_SRW_LOCK_H
-#define REAL_SRW_LOCK_H
+#ifndef REAL_SRW_LOCK_LL_H
+#define REAL_SRW_LOCK_LL_H
 
 #ifdef __cplusplus
 #include <cstdbool>
@@ -12,42 +12,38 @@
 
 #include "macro_utils/macro_utils.h"
 
-#include "c_pal/srw_lock.h"
+#include "c_pal/srw_lock_ll.h"
 
 #define R2(X) REGISTER_GLOBAL_MOCK_HOOK(X, real_##X);
 
-#define REGISTER_SRW_LOCK_GLOBAL_MOCK_HOOK() \
+#define REGISTER_SRW_LOCK_LL_GLOBAL_MOCK_HOOK() \
     MU_FOR_EACH_1(R2, \
-        srw_lock_create, \
-        srw_lock_destroy, \
-        srw_lock_acquire_exclusive, \
-        srw_lock_try_acquire_exclusive, \
-        srw_lock_release_exclusive, \
-        srw_lock_acquire_shared, \
-        srw_lock_try_acquire_shared, \
-        srw_lock_release_shared \
+        srw_lock_ll_init, \
+        srw_lock_ll_deinit, \
+        srw_lock_ll_acquire_exclusive, \
+        srw_lock_ll_try_acquire_exclusive, \
+        srw_lock_ll_release_exclusive, \
+        srw_lock_ll_acquire_shared, \
+        srw_lock_ll_try_acquire_shared, \
+        srw_lock_ll_release_shared \
 )
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+int real_srw_lock_ll_init(SRW_LOCK_LL* srw_lock_ll);
+void real_srw_lock_ll_deinit(SRW_LOCK_LL* srw_lock_ll);
 
-SRW_LOCK_HANDLE real_srw_lock_create(bool do_statistics, const char* lock_name);
+/*writer APIs*/
+void real_srw_lock_ll_acquire_exclusive(SRW_LOCK_LL* srw_lock_ll);
+SRW_LOCK_LL_TRY_ACQUIRE_RESULT real_srw_lock_ll_try_acquire_exclusive(SRW_LOCK_LL* srw_lock_ll);
+void real_srw_lock_ll_release_exclusive(SRW_LOCK_LL* srw_lock_ll);
 
-void real_srw_lock_destroy(SRW_LOCK_HANDLE handle);
-
-void real_srw_lock_acquire_exclusive(SRW_LOCK_HANDLE handle);
-
-SRW_LOCK_TRY_ACQUIRE_RESULT real_srw_lock_try_acquire_exclusive(SRW_LOCK_HANDLE handle);
-
-void real_srw_lock_release_exclusive(SRW_LOCK_HANDLE handle);
-
-void real_srw_lock_acquire_shared(SRW_LOCK_HANDLE handle);
-
-SRW_LOCK_TRY_ACQUIRE_RESULT real_srw_lock_try_acquire_shared(SRW_LOCK_HANDLE handle);
-
-void real_srw_lock_release_shared(SRW_LOCK_HANDLE handle);
+/*reader APIs*/
+void real_srw_lock_ll_acquire_shared(SRW_LOCK_LL* srw_lock_ll);
+SRW_LOCK_LL_TRY_ACQUIRE_RESULT real_srw_lock_ll_try_acquire_shared(SRW_LOCK_LL* srw_lock_ll);
+void real_srw_lock_ll_release_shared(SRW_LOCK_LL* srw_lock_ll);
 
 #ifdef __cplusplus
 }
--- interfaces/reals/real_srw_lock_ll_renames.h
+++ interfaces/reals/real_srw_lock_ll_renames.h
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define srw_lock_create real_srw_lock_create
-#define srw_lock_destroy real_srw_lock_destroy
-#define srw_lock_acquire_exclusive real_srw_lock_acquire_exclusive
-#define srw_lock_try_acquire_exclusive real_srw_lock_try_acquire_exclusive
-#define srw_lock_release_exclusive real_srw_lock_release_exclusive
-#define srw_lock_acquire_shared real_srw_lock_acquire_shared
-#define srw_lock_try_acquire_shared real_srw_lock_try_acquire_shared
-#define srw_lock_release_shared real_srw_lock_release_shared
+#define srw_lock_ll_init real_srw_lock_ll_init
+#define srw_lock_ll_deinit real_srw_lock_ll_deinit
+#define srw_lock_ll_acquire_exclusive real_srw_lock_ll_acquire_exclusive
+#define srw_lock_ll_try_acquire_exclusive real_srw_lock_ll_try_acquire_exclusive
+#define srw_lock_ll_release_exclusive real_srw_lock_ll_release_exclusive
+#define srw_lock_ll_acquire_shared real_srw_lock_ll_acquire_shared
+#define srw_lock_ll_try_acquire_shared real_srw_lock_ll_try_acquire_shared
+#define srw_lock_ll_release_shared real_srw_lock_ll_release_shared
