1bef451624b88c13c0ede4390024c8529439c102
Riddhi Gupta <riddhigupta@microsoft.com>
2024-04-04 15:42:02 +0530

    header and specs

--- interfaces/devdoc/malloc_multi_flex_requirements.md
+++ interfaces/devdoc/malloc_multi_flex_requirements.md
@@ -60,3 +60,20 @@ Note: the order of members specified in the `DEFINE_MALLOC_MULTI_FLEX` should ma
 ```
 
 `DEFINE_MALLOC_MULTI_FLEX` allows defining the memory allocation function for the `type` provided.
+
+**SRS_MALLOC_MULTI_FLEX_24_001: [** If the total amount of memory required to allocate the `type` along with its members exceeds `SIZE_MAX` then `DEFINE_MALLOC_MULTI_FLEX` shall fail and return `NULL`. **]**
+
+**SRS_MALLOC_MULTI_FLEX_24_002: [** `DEFINE_MALLOC_MULTI_FLEX` shall call `malloc` to allocate memory for the struct and its members. **]**
+
+**SRS_MALLOC_MULTI_FLEX_24_003: [** `DEFINE_MALLOC_MULTI_FLEX` shall assign address pointers to all the member arrays. **]**
+
+**SRS_MALLOC_MULTI_FLEX_24_004: [** `DEFINE_MALLOC_MULTI_FLEX` shall succeed and return the address returned by `malloc` function. **]**
+
+### MALLOC_MULTI_FLEX
+
+```c
+#define MALLOC_MULTI_FLEX(type) \
+    ...
+```
+
+**SRS_MALLOC_MULTI_FLEX_24_005: [** `MALLOC_MULTI_FLEX` shall expand `type` to the name of the malloc function in the format of: `MALLOC_MULTI_FLEX_type`. **]**
--- interfaces/inc/c_pal/malloc_multi_flex.h
+++ interfaces/inc/c_pal/malloc_multi_flex.h
@@ -39,16 +39,21 @@ extern "C" {
     #define ASSIGN_INTERNAL_STRUCT_PTRS(...) \
         MU_FOR_EACH_1(ASSIGN_INTERNAL_STRUCT_PTR, __VA_ARGS__)
 
+    //Codes_MALLOC_MULTI_FLEX_24_005: [ MALLOC_MULTI_FLEX shall expand type to the name of the malloc function in the format of: MALLOC_MULTI_FLEX_type. ]\
     #define MALLOC_MULTI_FLEX(type) \
         MU_C2(malloc_multi_flex, type) \
 
     #define DEFINE_MALLOC_MULTI_FLEX(type, ...)\
         static void* MALLOC_MULTI_FLEX(type)(size_t parent_struct_size ARG_LIST_VALUES(__VA_ARGS__))   \
         {\
+            //Codes_SRS_MALLOC_MULTI_FLEX_24_001: [ If the total amount of memory required to allocate the type along with its members exceeds SIZE_MAX then DEFINE_MALLOC_MULTI_FLEX shall fail and return NULL. ]\
             ARGS_OVERFLOW_CHECK(__VA_ARGS__)\
+            //Codes_SRS_MALLOC_MULTI_FLEX_24_002: [ DEFINE_MALLOC_MULTI_FLEX shall call malloc to allocate memory for the struct and its members. ]\
             type* parent_struct_pointer = malloc(parent_struct_size + size_tracker);\
             void* pointer_iterator = (char*)parent_struct_pointer + parent_struct_size; \
+            //Codes_SRS_MALLOC_MULTI_FLEX_24_003: [ DEFINE_MALLOC_MULTI_FLEX shall assign address pointers to all the member arrays. ]\
             ASSIGN_INTERNAL_STRUCT_PTRS(__VA_ARGS__)\
+            //Codes_SRS_MALLOC_MULTI_FLEX_24_004: [ DEFINE_MALLOC_MULTI_FLEX shall succeed and return the address returned by malloc. ]\
             return parent_struct_pointer;\
         }\
 
