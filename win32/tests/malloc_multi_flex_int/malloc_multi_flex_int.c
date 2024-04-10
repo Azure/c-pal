190145f097106cf68bf268e54c630d21733b3b92
Riddhi Gupta <riddhigupta@microsoft.com>
2024-04-10 16:44:14 +0530

    Addressing comments

--- win32/tests/malloc_multi_flex_int/malloc_multi_flex_int.c
+++ win32/tests/malloc_multi_flex_int/malloc_multi_flex_int.c
@@ -5,6 +5,9 @@
 
 #include "testrunnerswitcher.h"
 
+#include "c_pal/gballoc_hl.h"
+#include "c_pal/gballoc_hl_redirect.h"
+
 #include "c_pal/malloc_multi_flex.h"
 
 BEGIN_TEST_SUITE(TEST_SUITE_NAME_FROM_CMAKE)
@@ -14,6 +17,14 @@ TEST_SUITE_INITIALIZE(TestClassInitialize)
 }
 
 TEST_SUITE_CLEANUP(TestClassCleanup)
+{
+}
+
+TEST_FUNCTION_INITIALIZE(TestMethodInitialize)
+{
+}
+
+TEST_FUNCTION_CLEANUP(TestMethodCleanup)
 {
 }
 
@@ -27,24 +38,18 @@ typedef struct INNER_STURCT_TAG
     uint64_t inner_int_2;
 }INNER_STRUCT;
 
-typedef struct PARENT_STRUCT_TAG
-{
-    uint64_t int_1;
-    uint32_t* array_1;
-    uint32_t int_2;
-    uint64_t* array_2;
-    INNER_STRUCT* array_3;
-    uint32_t int_3;
-}PARENT_STRUCT;
+DECLARE_MALLOC_MULTI_FLEX(PARENT_STRUCT,
+    FIELDS(uint64_t, int_1, uint32_t, int_2, uint32_t, int_3),
+    ARRAY_FIELDS(uint32_t, array_1, uint64_t, array_2, INNER_STRUCT, array_3))
 
-DEFINE_MALLOC_MULTI_FLEX(PARENT_STRUCT, uint32_t, array_1, uint64_t, array_2, INNER_STRUCT, array_3)
+DEFINE_MALLOC_MULTI_FLEX(PARENT_STRUCT,
+    FIELDS(uint64_t, int_1, uint32_t, int_2, uint32_t, int_3),
+    ARRAY_FIELDS(uint32_t, array_1, uint64_t, array_2, INNER_STRUCT, array_3))
 
-TEST_FUNCTION_INITIALIZE(TestMethodInitialize)
+    static void set_all_bits(PARENT_STRUCT* test_struct_handle)
 {
-}
-
-TEST_FUNCTION_CLEANUP(TestMethodCleanup)
-{
+    size_t bytes_to_set = (array1_size * sizeof(uint32_t)) + alignof(uint32_t) - 1 + (array2_size * sizeof(uint64_t)) + alignof(uint64_t) - 1 + (array3_size * sizeof(INNER_STRUCT)) + alignof(INNER_STRUCT) - 1;
+    memset(test_struct_handle + sizeof(PARENT_STRUCT), 0xFF, bytes_to_set);
 }
 
 static void assign_struct(PARENT_STRUCT* test_struct_handle)
@@ -103,35 +108,12 @@ TEST_FUNCTION(test_malloc_multi_flex_allocates_memory_and_assigns_address_ptr_fo
     //arrange
 
     //act
-    PARENT_STRUCT* test_struct_handle = MALLOC_MULTI_FLEX(PARENT_STRUCT)(sizeof(PARENT_STRUCT), 10, 20, 30);
+    PARENT_STRUCT* test_struct_handle = MALLOC_MULTI_FLEX(PARENT_STRUCT)(sizeof(PARENT_STRUCT), array1_size, array2_size, array3_size);
+    set_all_bits(test_struct_handle);
     assign_struct(test_struct_handle);
 
     //assert
     assert_struct(test_struct_handle);
-
-    //cleanup
-    free(test_struct_handle);
-}
-
-DEFINE_MALLOC_MULTI_FLEX(INNER_STRUCT)
-
-/* Tests_SRS_MALLOC_MULTI_FLEX_24_001: [ If the total amount of memory required to allocate the type along with its members exceeds SIZE_MAX then DEFINE_MALLOC_MULTI_FLEX shall fail and return NULL. ]*/
-/* Tests_SRS_MALLOC_MULTI_FLEX_24_002: [ DEFINE_MALLOC_MULTI_FLEX shall call malloc to allocate memory for the struct and its members. ]*/
-/* Tests_SRS_MALLOC_MULTI_FLEX_24_003: [ DEFINE_MALLOC_MULTI_FLEX shall assign address pointers to all the member arrays. ]*/
-/* Tests_SRS_MALLOC_MULTI_FLEX_24_004: [ DEFINE_MALLOC_MULTI_FLEX shall succeed and return the address returned by malloc. ]*/
-/* Tests_MALLOC_MULTI_FLEX_24_005: [ MALLOC_MULTI_FLEX shall expand type to the name of the malloc function in the format of: MALLOC_MULTI_FLEX_type. ]*/
-TEST_FUNCTION(test_malloc_multi_flex_works_for_struct_with_no_array_members)
-{
-    //arrange
-
-    //act
-    INNER_STRUCT* test_struct_handle = MALLOC_MULTI_FLEX(INNER_STRUCT)(sizeof(INNER_STRUCT));
-    test_struct_handle->inner_int_1 = 3;
-    test_struct_handle->inner_int_2 = 6;
-
-    //assert
-    ASSERT_ARE_EQUAL(int, 3, test_struct_handle->inner_int_1);
-    ASSERT_ARE_EQUAL(int, 6, test_struct_handle->inner_int_2);
 
     //cleanup
     free(test_struct_handle);
