28f93791b4d3194c6f0a5cb6fc6243e40ff7400d
Andrei Porumb <anporumb@microsoft.com>
2024-05-08 13:57:48 -0700

    no leaks

--- common/tests/thandle_int/thandle_ptr_user.h
+++ common/tests/thandle_int/thandle_ptr_user.h
@@ -24,24 +24,29 @@
 #define THANDLE_PTR_FREE_FUNC_TYPE(T) typedef void (*THANDLE_PTR_FREE_FUNC_TYPE_NAME(T))(T arg);
 
 /*this introduces a new *type* which is a structure with a field of type T"*/
-#define PTR_STRUCT_TYPE_TYPEDEF(T) typedef struct PTR_STRUCT_TAG_TYPE_NAME(T) {T pointer;} PTR_STRUCT_TYPE_NAME(T);
+#define PTR_STRUCT_TYPE_TYPEDEF(T) typedef struct PTR_STRUCT_TAG_TYPE_NAME(T) {T pointer; THANDLE_PTR_FREE_FUNC_TYPE_NAME(T) the_free;} PTR_STRUCT_TYPE_NAME(T);
 
 /*this introduces one new *name* for a function which is used to capture a T ptr and move it under the THANDLE(PTR(T))*/
 #define THANDLE_PTR_CREATE_NAME(T) MU_C2(THANDLE_PTR_CREATE_, T)
 
 /*this introduces the declaration of a function that returns a THANDLE(PTR_STRUCT_TYPE_NAME(T))*/
 #define THANDLE_PTR_DECLARE(T) \
+    THANDLE_PTR_FREE_FUNC_TYPE(T);                                                                                  \
     PTR_STRUCT_TYPE_TYPEDEF(T);                                                                                     \
-    /*THANDLE_PTR_FREE_FUNC_TYPE(T);*/                                                                              \
     THANDLE_TYPE_DECLARE(PTR_STRUCT_TYPE_NAME(T));                                                                  \
 
 /*this introduces the definition of the function declared above*/
 #define THANDLE_PTR_DEFINE(T) \
     THANDLE_TYPE_DEFINE(PTR_STRUCT_TYPE_NAME(T));                                                                   \
-    THANDLE(PTR_STRUCT_TYPE_NAME(T)) create(T source)                                                               \
+    static void inner_dispose(PTR_STRUCT_TYPE_NAME(T)* ptr)                                                         \
     {                                                                                                               \
-        PTR_STRUCT_TYPE_NAME(T) temp = { .pointer = source };                                                       \
-        return THANDLE_CREATE_FROM_CONTENT(PTR_STRUCT_TYPE_NAME(T))(&temp, NULL, NULL);                             \
+        ptr->the_free(ptr->pointer);                                                                                \
+    }                                                                                                               \
+    THANDLE(PTR_STRUCT_TYPE_NAME(T)) create(T source, void(*dispose)(T) )                                           \
+    {                                                                                                               \
+        (void)dispose;                                                                                              \
+        PTR_STRUCT_TYPE_NAME(T) temp = { .pointer = source, .the_free=dispose };                                    \
+        return THANDLE_CREATE_FROM_CONTENT(PTR_STRUCT_TYPE_NAME(T))(&temp, inner_dispose, NULL);                    \
     }
 
 #endif /*THANDLE_PTR_USER_H*/
