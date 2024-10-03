47c9023aed03a5b3fd9210802572aca71f989769
Bert Kleewein <bertk@microsoft.com>
2024-10-03 13:16:45 -0700

    PR feedback

--- win32/inc/c_pal/job_object_helper.h
+++ win32/inc/c_pal/job_object_helper.h
@@ -13,20 +13,18 @@
 
 #include "macro_utils/macro_utils.h"
 
-#include "umock_c/umock_c_prod.h"
-
 #include "c_pal/thandle.h"
 
 typedef struct JOB_OBJECT_HELPER_TAG JOB_OBJECT_HELPER;
 THANDLE_TYPE_DECLARE(JOB_OBJECT_HELPER);
+
+#include "umock_c/umock_c_prod.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
     MOCKABLE_FUNCTION(, THANDLE(JOB_OBJECT_HELPER), job_object_helper_create);
-    MOCKABLE_FUNCTION(, int, job_object_helper_open, THANDLE(JOB_OBJECT_HELPER), job_object_helper);
-    MOCKABLE_FUNCTION(, void, job_object_helper_close, THANDLE(JOB_OBJECT_HELPER), job_object_helper);
     MOCKABLE_FUNCTION(, int, job_object_helper_limit_memory, THANDLE(JOB_OBJECT_HELPER), job_object_helper, uint32_t, percent_physical_memory);
     MOCKABLE_FUNCTION(, int, job_object_helper_limit_cpu, THANDLE(JOB_OBJECT_HELPER), job_object_helper, uint32_t, percent_cpu);
 #ifdef __cplusplus
--- win32/src/job_object_helper.c
+++ win32/src/job_object_helper.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include "stdint.h"
+#include <stdint.h>
 
 #include "macro_utils/macro_utils.h"
 #include "c_logging/logger.h"
@@ -9,20 +9,14 @@
 #include "c_pal/job_object_helper.h"
 
 
+static void job_object_helper_dispose(JOB_OBJECT_HELPER* job_object_helper)
+{
+    (void) job_object_helper;
+}
+
 IMPLEMENT_MOCKABLE_FUNCTION(, THANDLE(JOB_OBJECT_HELPER), job_object_helper_create)
 {
     return NULL;
-}
-
-IMPLEMENT_MOCKABLE_FUNCTION(, int, job_object_helper_open, THANDLE(JOB_OBJECT_HELPER), job_object_helper)
-{
-    (void) job_object_helper;
-    return MU_FAILURE;
-}
-
-IMPLEMENT_MOCKABLE_FUNCTION(, void, job_object_helper_close, THANDLE(JOB_OBJECT_HELPER), job_object_helper)
-{
-    (void) job_object_helper;
 }
 
 IMPLEMENT_MOCKABLE_FUNCTION(, int, job_object_helper_limit_memory, THANDLE(JOB_OBJECT_HELPER), job_object_helper, uint32_t, percent_physical_memory)
