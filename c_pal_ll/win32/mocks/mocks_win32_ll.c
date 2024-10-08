13406b6f8f21d5dcb34958b935318092b3f0076c
Andrei Porumb <anporumb@microsoft.com>
2024-10-08 14:16:39 -0700

    this creates by hand win32_ll_mocks and uses it in call_once_ut_exe_c_pal

--- c_pal_ll/win32/mocks/mocks_win32_ll.c
+++ c_pal_ll/win32/mocks/mocks_win32_ll.c
@@ -1,8 +1,5 @@
-
-#include "mocks_win32_ll.h"
-
-#define ENABLE_MOCKS_IMPL
+#define ENABLE_MOCKS
 #include "c_pal/interlocked.h"
 #include "c_pal/sync.h"
 #include "c_pal/threadapi.h"
-#undef ENABLE_MOCKS_IMPL
+#undef ENABLE_MOCKS
--- c_pal_ll/win32/mocks/mocks_win32_ll.h
+++ c_pal_ll/win32/mocks/mocks_win32_ll.h
@@ -1,10 +1,12 @@
 #ifndef MOCKS_WIN32_LL_H
 #define MOCKS_WIN32_LL_H
 
+#define ENABLE_MOCKS
 #define ENABLE_MOCKS_DECL
 #include "c_pal/interlocked.h"
 #include "c_pal/sync.h"
 #include "c_pal/threadapi.h"
 #undef ENABLE_MOCKS_DECL
+#undef ENABLE_MOCKS
 
 #endif // MOCKS_WIN32_LL_H
