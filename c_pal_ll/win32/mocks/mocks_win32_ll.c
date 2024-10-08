d1d76193062ab484b97796c31a4ae3cf01979c28
Andrei Porumb <anporumb@microsoft.com>
2024-10-08 10:11:57 -0700

    meek attempt to have a mocks library with precompiled header

---
+++ c_pal_ll/win32/mocks/mocks_win32_ll.c
@@ -0,0 +1,8 @@
+
+#include "mocks_win32_ll.h"
+
+#define ENABLE_MOCKS_IMPL
+#include "c_pal/interlocked.h"
+#include "c_pal/sync.h"
+#include "c_pal/threadapi.h"
+#undef ENABLE_MOCKS_IMPL
---
+++ c_pal_ll/win32/mocks/mocks_win32_ll.h
@@ -0,0 +1,10 @@
+#ifndef MOCKS_WIN32_LL_H
+#define MOCKS_WIN32_LL_H
+
+#define ENABLE_MOCKS_DECL
+#include "c_pal/interlocked.h"
+#include "c_pal/sync.h"
+#include "c_pal/threadapi.h"
+#undef ENABLE_MOCKS_DECL
+
+#endif // MOCKS_WIN32_LL_H
