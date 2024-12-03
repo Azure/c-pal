f7773feeca49c6e8351bdf4a9cc8ef6f863f23c6
Jelani Brandon <jelani.brandon@microsoft.com>
2024-12-03 15:19:41 -0800

    add function to get local address name (#397)
    * adding code to get local address information from the service
    
    ---------
    
    Co-authored-by: Jelani Brandon <jebrando@microsoft.com>

--- common/reals/real_socket_transport_renames.h
+++ common/reals/real_socket_transport_renames.h
@@ -12,9 +12,11 @@
 #define socket_transport_receive                   real_socket_transport_receive
 #define socket_transport_get_underlying_socket     real_socket_transport_get_underlying_socket
 #define socket_transport_is_valid_socket           real_socket_transport_is_valid_socket
+#define socket_transport_get_local_address         real_socket_transport_get_local_address
 
 #define SOCKET_SEND_RESULT                         real_SOCKET_SEND_RESULT
 #define SOCKET_RECEIVE_RESULT                      real_SOCKET_RECEIVE_RESULT
 #define SOCKET_ACCEPT_RESULT                       real_SOCKET_ACCEPT_RESULT
 #define SOCKET_TYPE                                real_SOCKET_TYPE
 #define SOCKET_IO_TYPE                             real_SOCKET_IO_TYPE
+#define ADDRESS_TYPE                               real_ADDRESS_TYPE
