88610d44c66e517c18e9203b2759c4da74ee8722
Shrikant Jadhav <shrjadhav@microsoft.com>
2025-07-17 18:47:34 +0530

    task-32704240 remove old apis from job object helper

--- win32/reals/real_job_object_helper.h
+++ win32/reals/real_job_object_helper.h
@@ -18,9 +18,7 @@
 
 #define REGISTER_JOB_OBJECT_HELPER_GLOBAL_MOCK_HOOK()          \
     MU_FOR_EACH_1(R2,                                   \
-        job_object_helper_create, \
-        job_object_helper_limit_memory, \
-        job_object_helper_limit_cpu \
+        job_object_helper_set_job_limits_to_current_process \
     ) \
     REGISTER_GLOBAL_MOCK_HOOK(THANDLE_MOVE(JOB_OBJECT_HELPER), THANDLE_MOVE(real_JOB_OBJECT_HELPER)) \
     REGISTER_GLOBAL_MOCK_HOOK(THANDLE_INITIALIZE(JOB_OBJECT_HELPER), THANDLE_INITIALIZE(real_JOB_OBJECT_HELPER)) \
@@ -33,12 +31,6 @@ extern "C" {
 
     typedef struct JOB_OBJECT_HELPER_TAG real_JOB_OBJECT_HELPER;
     THANDLE_TYPE_DECLARE(real_JOB_OBJECT_HELPER);
-
-    THANDLE(JOB_OBJECT_HELPER) real_job_object_helper_create(void);
-
-    int real_job_object_helper_limit_memory(THANDLE(JOB_OBJECT_HELPER) job_object_helper, uint32_t percent_physical_memory);
-
-    int real_job_object_helper_limit_cpu(THANDLE(JOB_OBJECT_HELPER) job_object_helper, uint32_t percent_cpu);
 
     THANDLE(JOB_OBJECT_HELPER) real_job_object_helper_set_job_limits_to_current_process(const char* job_name, uint32_t percent_cpu, uint32_t percent_physical_memory);
 
