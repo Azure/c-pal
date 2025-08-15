43a820ef67cce3e55742f055fb5315bef2ad7847
Dan Cristoloveanu <dcristo@microsoft.com>
2025-08-15 13:02:38 -0700

    Fix string_utils warnings with latest compiler

--- win32/inc/c_pal/string_utils.h
+++ win32/inc/c_pal/string_utils.h
@@ -37,12 +37,11 @@ char* sprintf_char_function(const char* format, ...);
 wchar_t* sprintf_wchar_function(const wchar_t* format, ...);
 #define sprintf_wchar(format, ...) (0?wprintf((format), ##__VA_ARGS__):0, sprintf_wchar_function((format), ##__VA_ARGS__))
 
-MOCKABLE_INTERFACE(string_utils_printf,
-    /*produces a string as if printed by vprintf*/
-    FUNCTION(, char*, vsprintf_char, const char*, format, va_list, va),
-    /*produces a string as if printed by vwprintf*/
-    FUNCTION(, wchar_t*, vsprintf_wchar, const wchar_t*, format, va_list, va)
-)
+/*produces a string as if printed by vprintf*/
+MOCKABLE_FUNCTION(, char*, vsprintf_char, const char*, format, va_list, va);
+
+/*produces a string as if printed by vwprintf*/
+MOCKABLE_FUNCTION(, wchar_t*, vsprintf_wchar, const wchar_t*, format, va_list, va);
 
 /*takes a FILETIME, returns a nice string representation of it*/
 MOCKABLE_FUNCTION(, char*, FILETIME_toAsciiArray, const FILETIME*, fileTime);
@@ -50,13 +49,11 @@ MOCKABLE_FUNCTION(, char*, FILETIME_toAsciiArray, const FILETIME*, fileTime);
 /*takes a FILETIME, returns a string in UTC time zone*/
 MOCKABLE_FUNCTION(, char*, FILETIME_to_string_UTC, const FILETIME*, fileTime);
 
-MOCKABLE_INTERFACE(string_utils_convert,
-    /*produces the wchar_t* string representation of source (which is assumed to be multibyte). Returns NULL on any failure.*/
-    FUNCTION(, wchar_t*, mbs_to_wcs, const char*, source),
+/*produces the wchar_t* string representation of source (which is assumed to be multibyte). Returns NULL on any failure.*/
+MOCKABLE_FUNCTION(, wchar_t*, mbs_to_wcs, const char*, source);
 
-    /*produces the multibyte char* string representation of source. Returns NULL on any failure.*/
-    FUNCTION(, char*, wcs_to_mbs, const wchar_t*, source)
-)
+/*produces the multibyte char* string representation of source. Returns NULL on any failure.*/
+MOCKABLE_FUNCTION(, char*, wcs_to_mbs, const wchar_t*, source);
 
 #ifdef __cplusplus
 }
