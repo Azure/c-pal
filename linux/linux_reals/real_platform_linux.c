aaf255222507500e18e546df9efa74ec7125c4e3
Parth Aggarwal <paaggarwal@microsoft.com>
2025-11-11 14:19:32 -0800

    add platform_linux reals

---
+++ linux/linux_reals/real_platform_linux.c
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "real_completion_port_linux_renames.h"
+
+#include "real_platform_linux_renames.h" // IWYU pragma: keep
+
+#include "../src/platform_linux.c"  // IWYU pragma: keep
