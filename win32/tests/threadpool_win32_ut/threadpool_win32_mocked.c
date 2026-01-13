766ca7c9dc552e546460bca76c1c20b9de65a930
Parth Aggarwal <paaggarwal@microsoft.com>
2026-01-13 15:27:17 -0800

    wip

--- win32/tests/threadpool_win32_ut/threadpool_win32_mocked.c
+++ win32/tests/threadpool_win32_ut/threadpool_win32_mocked.c
@@ -5,35 +5,30 @@
 #pragma warning(disable: 4273)
 
 #define CreateThreadpoolWork mocked_CreateThreadpoolWork
-#define CreateThreadpoolTimer mocked_CreateThreadpoolTimer
 #define InitializeThreadpoolEnvironment mocked_InitializeThreadpoolEnvironment
 #define SetThreadpoolCallbackPool mocked_SetThreadpoolCallbackPool
 #define CreateThreadpoolCleanupGroup mocked_CreateThreadpoolCleanupGroup
 #define SetThreadpoolCallbackCleanupGroup mocked_SetThreadpoolCallbackCleanupGroup
 #define CloseThreadpoolCleanupGroupMembers mocked_CloseThreadpoolCleanupGroupMembers
 #define CloseThreadpoolWork mocked_CloseThreadpoolWork
-#define CloseThreadpoolTimer mocked_CloseThreadpoolTimer
 #define CloseThreadpoolCleanupGroup mocked_CloseThreadpoolCleanupGroup
 #define DestroyThreadpoolEnvironment mocked_DestroyThreadpoolEnvironment
 #define SubmitThreadpoolWork mocked_SubmitThreadpoolWork
-#define SetThreadpoolTimer mocked_SetThreadpoolTimer
-#define WaitForThreadpoolTimerCallbacks mocked_WaitForThreadpoolTimerCallbacks
 #define WaitForThreadpoolWorkCallbacks mocked_WaitForThreadpoolWorkCallbacks
 
 PTP_WORK mocked_CreateThreadpoolWork(PTP_WORK_CALLBACK pfnwk, PVOID pv, PTP_CALLBACK_ENVIRON pcbe);
-PTP_TIMER mocked_CreateThreadpoolTimer(PTP_TIMER_CALLBACK pfnti, PVOID pv, PTP_CALLBACK_ENVIRON pcbe);
 void mocked_InitializeThreadpoolEnvironment(PTP_CALLBACK_ENVIRON pcbe);
 void mocked_SetThreadpoolCallbackPool(PTP_CALLBACK_ENVIRON pcbe, PTP_POOL ptpp);
 PTP_CLEANUP_GROUP mocked_CreateThreadpoolCleanupGroup(void);
 VOID mocked_SetThreadpoolCallbackCleanupGroup(PTP_CALLBACK_ENVIRON pcbe, PTP_CLEANUP_GROUP ptpcg, PTP_CLEANUP_GROUP_CANCEL_CALLBACK pfng);
 void mocked_CloseThreadpoolCleanupGroupMembers(PTP_CLEANUP_GROUP ptpcg, BOOL fCancelPendingCallbacks, PVOID pvCleanupContext);
 void mocked_CloseThreadpoolWork(PTP_WORK pwk);
-void mocked_CloseThreadpoolTimer(PTP_TIMER pti);
 void mocked_CloseThreadpoolCleanupGroup(PTP_CLEANUP_GROUP ptpcg);
 void mocked_DestroyThreadpoolEnvironment(PTP_CALLBACK_ENVIRON pcbe);
 void mocked_SubmitThreadpoolWork(PTP_WORK pwk);
-void mocked_SetThreadpoolTimer(PTP_TIMER pti, PFILETIME pftDueTime, DWORD msPeriod, DWORD msWindowLength);
-void mocked_WaitForThreadpoolTimerCallbacks(PTP_TIMER pti, BOOL fCancelPendingCallbacks);
 void mocked_WaitForThreadpoolWorkCallbacks(PTP_WORK pwk, BOOL fCancelPendingCallbacks);
+
+// Timer mock prototypes from shared header
+#include "mock_threadpool_timer.h"
 
 #include "../../src/threadpool_win32.c"
