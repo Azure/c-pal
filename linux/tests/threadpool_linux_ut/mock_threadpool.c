4ef4dd3414efc97c6469df97a21b6fe1b0256605
Parth Aggarwal <paaggarwal@microsoft.com>
2026-01-15 12:52:43 -0800

    undo unnecessary changes

--- linux/tests/threadpool_linux_ut/mock_threadpool.c
+++ linux/tests/threadpool_linux_ut/mock_threadpool.c
@@ -4,16 +4,21 @@
 #include <semaphore.h>
 #include <bits/types/timer_t.h>
 
-// Timer mocks from shared header
-#include "mock_timer.h"
-
+#define timer_create mocked_timer_create
+#define timer_settime mocked_timer_settime
+#define timer_delete mocked_timer_delete
 #define sem_init mocked_sem_init
 #define sem_post mocked_sem_post
 #define sem_timedwait mocked_sem_timedwait
 #define clock_gettime mocked_clock_gettime
 
+struct itimerspec;
+struct sigevent;
 struct timespec;
 
+int mocked_timer_create(clockid_t clockid, struct sigevent* restrict sevp, timer_t* restrict timerid);
+int mocked_timer_settime(timer_t timerid, int flags, const struct itimerspec* new_value, struct itimerspec* old_value);
+int mocked_timer_delete(timer_t timerid);
 int mocked_sem_post(sem_t* sem);
 int mocked_sem_init(sem_t* sem, int pshared, unsigned int value);
 int mocked_sem_timedwait(sem_t* sem, const struct timespec* abs_timeout);
--- win32/tests/mocked/inc/windows.h
+++ win32/tests/mocked/inc/windows.h
@@ -3,11 +3,6 @@
 #ifndef MOCKED_WINDOWS_H
 #define MOCKED_WINDOWS_H
 
-// Define _WINDOWS_ to let mock headers know windows.h has been included
-#ifndef _WINDOWS_
-#define _WINDOWS_
-#endif
-
 #include <stdint.h>
 
 typedef void *HANDLE; /*works for most of the cases, except for those sneaky handles returned by CreateFile for which INVALID_HANDLE_VALUE is "the NULL"*/
@@ -17,49 +12,18 @@ typedef void *HANDLE; /*works for most of the cases, except for those sneaky han
 typedef void* LONG_PTR; /*approximatively*/
 
 typedef void* PTP_IO;
-typedef void* PTP_POOL;
-typedef void* PTP_TIMER;
-typedef void* PTP_WORK;
-typedef void* PTP_CLEANUP_GROUP;
-typedef void* PTP_CALLBACK_ENVIRON;
-typedef void* PTP_CALLBACK_INSTANCE;
-typedef void* PFILETIME;
 
 typedef int64_t LONGLONG;
 typedef int64_t LONG64;
 typedef int32_t LONG;
 typedef unsigned long ULONG;
 typedef uint64_t ULONG64;
-typedef uint64_t ULONGLONG;
 typedef LONG HRESULT;
 typedef unsigned long DWORD;
 typedef void VOID;
 typedef void* PVOID;
 typedef size_t SIZE_T;
 typedef long BOOL;
-
-#define CALLBACK
-
-typedef VOID (CALLBACK *PTP_TIMER_CALLBACK)(PTP_CALLBACK_INSTANCE, PVOID, PTP_TIMER);
-typedef VOID (CALLBACK *PTP_WORK_CALLBACK)(PTP_CALLBACK_INSTANCE, PVOID, PTP_WORK);
-typedef VOID (CALLBACK *PTP_CLEANUP_GROUP_CANCEL_CALLBACK)(PVOID, PVOID);
-
-typedef union _ULARGE_INTEGER {
-    struct {
-        DWORD LowPart;
-        DWORD HighPart;
-    };
-    struct {
-        DWORD LowPart;
-        DWORD HighPart;
-    } u;
-    ULONGLONG QuadPart;
-} ULARGE_INTEGER;
-
-typedef struct _FILETIME {
-    DWORD dwLowDateTime;
-    DWORD dwHighDateTime;
-} FILETIME;
 
 #define FALSE 0
 #define TRUE 1
